240856afc7cb4a0aa42eef7c2e3afbaa5ce1f16c
Harry Dobbs <hrdobbs@outlook.com>
2022-09-12 06:23:13 +0100

    complete to step 95

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,10 +25,7 @@ const Spotify = {
             window.history.pushState('Access Token', null, '/');
             return accessToken; 
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?
-            client_id=${clientID}
-            &response_type=token&scope=playlist-modify
-            -public&redirect_uri=${redirectURI}`;
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
             window.location = accessUrl;
 
         }
@@ -63,6 +60,27 @@ const Spotify = {
         const accessToken = Spotify.accessToken;
         const headers = {Authorizarion : `Bearer ${accessToken}`};
         let userId;
+
+        return fetch(`https://api.spotify.com/v1/me`, {
+            headers: headers
+        }).then (response => {
+            response.json()
+        }).then (jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch (`https://api.spotify.com/v1/users/${userId}/playlists`, {
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({name: name})
+            }).then( response => response.json()
+            ).then(jsonResponse => {
+                const playlistId = jsonResponse.id;
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({ uris : trackUris})
+                })
+            })
+        })
     }
 
 }
